dcf2bcec06d0ac60a9cd1e2da6c4f66dd74bcdcc
Alex <temper8@gmail.com>
2025-05-25 22:32:02 +0300

    Update test.py

--- test.py
+++ test.py
@@ -6,12 +6,27 @@ app = marimo.App(width="medium")
 
 @app.cell
 def _():
-    a = 3
-    b = 4
-    a + b
+    import numpy as np
+    import marimo as mo
+    from matplotlib import pyplot as plt
+    return mo, np, plt
+
+
+@app.cell
+def _(mo):
+    omega = mo.ui.slider(1,9)
+    omega
+    return (omega,)
+
+
+@app.cell
+def _(np, omega, plt):
+    x=np.linspace(0., 1., 30)
+    plt.plot(x, np.sin(omega.value*x), label='sin')
+    plt.legend()
+    plt.show()
     return
 
 
 if __name__ == "__main__":
     app.run()
-
