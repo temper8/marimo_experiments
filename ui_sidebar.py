79f759101d8a30fb275b0b3e17ccff55bfe077d6
Alex <temper8@gmail.com>
2025-07-02 12:03:46 +0300

    Update ui_sidebar.py

--- ui_sidebar.py
+++ ui_sidebar.py
@@ -7,23 +7,7 @@ app = marimo.App(width="medium")
 @app.cell
 def _():
     import marimo as mo
-    mo.sidebar(
-        [
-            mo.md("# Wave 2D"),
-            mo.nav_menu(
-                {
-                    "#home": f"{mo.icon('lucide:home')} Home",
-                    "#about": f"{mo.icon('lucide:user')} About",
-                    "#contact": f"{mo.icon('lucide:phone')} Contact",
-                    "Links": {
-                        "https://twitter.com/marimo_io": "Twitter",
-                        "https://github.com/marimo-team/marimo": "GitHub",
-                    },
-                },
-                orientation="vertical",
-            ),
-        ]
-    )
+
     return (mo,)
 
 
@@ -75,36 +59,66 @@ def _(folder_browser, mo):
 
 @app.cell
 def _(folder_browser, mo, task_browser):
-
-    mo.hstack(
+    mo.sidebar(
         [
-           folder_browser,
-           task_browser,
-        ]
+            mo.md("# Wave 2D"),
+            mo.nav_menu(
+                {
+                    "#home": f"{mo.icon('lucide:home')} Home",
+                    "#about": f"{mo.icon('lucide:user')} About",
+                    "#contact": f"{mo.icon('lucide:phone')} Contact",
+                    "Links": {
+                        "https://twitter.com/marimo_io": "Twitter",
+                        "https://github.com/marimo-team/marimo": "GitHub",
+                    },
+                },
+                orientation="vertical",
+            ),
+        mo.accordion(
+        {
+            "Door 1": folder_browser,
+            "Door 2": task_browser,
+            "Door 3": mo.md(
+                "![goat](https://images.unsplash.com/photo-1524024973431-2ad916746881)"
+            ),
+        }
+        )
+                ]
     )
+
     return
 
 
 @app.cell
 def _(mo):
     mo.md(
-        r'''
-        The exponential function $f(x) = e^x$ can be represented as
+        r"""
+    The exponential function $f(x) = e^x$ can be represented as
 
-        \[
-            f(x) = 1 + x + \frac{x^2}{2!} + \frac{x^3}{3!} + \ldots.
-        \]
-        '''
+    \[
+        f(x) = 1 + x + \frac{x^2}{2!} + \frac{x^3}{3!} + \ldots.
+    \]
+    """
     )
     return
 
 
 @app.cell
 def _(mo):
+    mo.md(r"""The exponential function $f(x) = e^x$ can be represented as""")
+    return
+
+
+@app.cell
+def _(mo):
     mo.md(
-        r'''
-        The exponential function $f(x) = e^x$ can be represented as
-        '''
+        r"""
+    /// details | Warning details  
+        type: warn
+
+    This highlights something to watch out for
+    ///
+    """
     )
     return
 
